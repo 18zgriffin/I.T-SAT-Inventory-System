688007dcc7b6a716cecc043334602f81261f07dc
XperiaZed <18zgriffin@mlmc.vic.edu.au>
2018-08-05 19:13:28 +1000

    adjusted and added comments to match current contents

--- Main.py
+++ Main.py
@@ -1,9 +1,8 @@
 from tkinter import *
 from tkinter.messagebox import showinfo
-import time
 
 class ConversionGUI:
-    #initials master
+    #initializes master
     def __init__(self, master):
         #names the window
         self.master = master
@@ -13,47 +12,47 @@ class ConversionGUI:
         self.titleText = Label(master, text="House Inventory - Login", font=("Times", "24", "bold italic"))
         self.titleText.grid(column = 0, columnspan = 3, row = 2, pady = 15)
 
-        # lable of highest number
+        #lable point for username
         self.feetLabel = Label(master, text="Username")
         self.feetLabel.grid(column=0, row=4)
-        # entry of the highest number
+        #point where the username is entered
         self.username = StringVar()
         self.entryValue = Entry(master, textvariable=self.username, width=15)
         self.entryValue.grid(row=5, column=0, padx=10)
 
-        #lable of lowest number
+        #lable of password
         self.feetLabel = Label(master, text="Password")
-        self.feetLabel.grid(column = 1, row = 4)
-        #entry of the lowest number
+        self.feetLabel.grid(column=1, row=4)
+        #point where the pasword is entered
         self.password = StringVar()
-        self.entryValue = Entry(master, textvariable=self.password, width = 15)
-        self.entryValue.grid(row=5, column = 1, padx = 10)
+        self.entryValue = Entry(master, textvariable=self.password, width=15)
+        self.entryValue.grid(row=5, column=1, padx=10)
 
-        #button that checks if
+        #this runs the submit command making the program check if the username and password is correct
         self.submitButton = Button(master, text="Login", command=self.submit)
-        self.submitButton.grid(row=6, column = 0, columnspan = 3)
+        self.submitButton.grid(row=6, column=0, columnspan=3)
 
         #button which closes the GUI
         self.close_button = Button(master, text="Exit", command=master.quit)
-        self.close_button.grid(row=7, column=1, sticky = "e")
+        self.close_button.grid(row=7, column=1, sticky="e")
 
-        #info button
+        #info button displays pages information
         self.button_showinfo = Button(master, text="?", command=self.loginInfoWindow)
-        self.button_showinfo.grid(column = 1, row=2, sticky = "e", pady = (0, 35))
+        self.button_showinfo.grid(column=1, row=2, sticky="e", pady=(0, 35))
 
-    #calcualate function, gathers all the information given from choices made in the GUI and uses them with other
-    #functions to them assign them back to the class and give results
+    #the login function, this gets the password and username database files and tests to see if the user input values
+    #match any of the logins
     def submit(self):
-        #assigns the class variables to variables for the calculation
+        #assigns the class variables to specfic variables for the calculation
         enterusername = self.username.get()
         enterpassword = self.password.get()
-        u = open("Usernames.txt", "r")
+        #opens the files and attaches them to a list each
+        u = open("usernames.txt", "r")
         ulist = [line.rstrip('\n') for line in u]
         p = open("passwords.txt", "r")
         plist = [line.rstrip('\n') for line in p]
-        print(ulist)
-        print(plist)
 
+        #tests to see if the login is found in the database
         x = 0
         for i in ulist:
             if enterusername == i:
@@ -68,6 +67,7 @@ class ConversionGUI:
             x = x + 1
         return
 
+    #function which runs the information window
     def loginInfoWindow(hi):
         showinfo("LoginHelp", "Enter a valid Username and Password into appropriate entry boxes")
 
